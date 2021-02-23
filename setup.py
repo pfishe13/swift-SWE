87b4d2e3069f8e796dfdf517abb9262dee11d1ab
pfishe13 <76025363+pfishe13@users.noreply.github.com>
2021-02-23 13:20:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     task_table = taskbook_db.create_table('task')
     task_table.insert_many([
         {"time":0.0, "sets":"3", "reps":"10", "description":"Do something dope", "list":"today", "completed":True},
-        {"time":0.5, "sets":"4", "reps":"12","description":"Do something fantastic", "list":"today", "completed":False},
+        {"time":0.5, "sets":"4", "reps":"12","description":"Do something dope", "list":"today", "completed":False},
         {"time":0.3, "sets":"5", "reps":"8","description":"Do something remarkable", "list":"tomorrow", "completed":False},
         {"time":0.7, "sets":"2", "reps":"15","description":"Do something unusual", "list":"tomorrow", "completed":True}
     ]) 
